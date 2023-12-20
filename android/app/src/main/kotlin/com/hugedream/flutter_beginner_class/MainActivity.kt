34a7f4c95da0c7283330cb12513ba1f43e7d62f8
Sangin Han <hugedreamhd@gmail.com>
2023-12-20 10:10:56 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/hugedream/flutter_beginner_class/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hugedream.flutter_beginner_class
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/hugedreamhd/flutter_beginner_class/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hugedreamhd.flutter_beginner_class
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
